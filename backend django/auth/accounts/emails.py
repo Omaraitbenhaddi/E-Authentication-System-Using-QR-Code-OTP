934ec857827e5cbc39010e698f81b97efa9f44ba
Halimabahtar <halimabahtar1993@gmail.com>
2022-08-21 20:39:23 +0200

    an other modification

--- backend django/auth/accounts/emails.py
+++ backend django/auth/accounts/emails.py
@@ -6,13 +6,13 @@ from .models import User
 def send_otp_email(email):
     if email:
         subject = 'Your account verification email'
-        cus="abcdefghijklmnopqrstuvwxyz1234567890"
+        cus="abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         length = len(cus)
         generate_otp=""
-        for i in range(5):
+        for i in range(10):
             generate_otp+= cus[math.floor(random.random() * length)]
         message =f'your otp is {generate_otp}'
-        email_from = settings.EMAIL_HOST_USER
+        email_from = settings.EMAIL_HOST_USERon 
         send_mail(subject, message, email_from , [email,])
         return generate_otp
     else:
