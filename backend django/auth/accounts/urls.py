e648b96b93b528365ce36f2c3fc3273cd6d14fcb
Halimabahtar <halimabahtar1993@gmail.com>
2022-08-27 19:24:12 +0200

    add a url

--- backend django/auth/accounts/urls.py
+++ backend django/auth/accounts/urls.py
@@ -1,20 +1,11 @@
 from django.urls import re_path
-from .views import LoginAPI, LogoutView, ValidateEmailSendOTP,Validateotp
+from .views import LoginAPI, LogoutView, ValidateEmailSendOTP,Validateotp, Welecom
 app_name='accounts'
 urlpatterns = [
-
-
-
     re_path(r'validate/', ValidateEmailSendOTP.as_view()),
     re_path(r'validate_otp/',Validateotp.as_view()),
     re_path(r'login/',LoginAPI.as_view()),
     re_path(r'logout/',LogoutView.as_view()),
-
-    
-
-
-
-
-   
+    re_path(r'myaccount/',Welcome.as_view()),  
 ]
 
