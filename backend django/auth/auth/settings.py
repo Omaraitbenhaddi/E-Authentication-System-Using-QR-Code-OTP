62f6f6f5c44a137ee2e3e66db319806aa7db3112
Halimabahtar <halimabahtar1993@gmail.com>
2022-08-27 19:39:41 +0200

    views

--- backend django/auth/auth/settings.py
+++ backend django/auth/auth/settings.py
@@ -145,5 +145,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'email@gmail.com'
+EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
