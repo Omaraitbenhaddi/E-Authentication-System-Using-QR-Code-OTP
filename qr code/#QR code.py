8c820f26858f144bf34d1e84fd9df82655a7247b
chaymae ait ahmed <95868338+chaymae2021@users.noreply.github.com>
2022-09-27 10:09:15 +0100

    Update #QR code.py

--- qr code/#QR code.py
+++ qr code/#QR code.py
@@ -29,9 +29,40 @@ info=user+" "+dt_string
 # Fernet module is imported from the
 # cryptography package
 from cryptography.fernet import Fernet
-
+import rsa
 # key is generated
 key = Fernet.generate_key()
+#coding by RSA the key
+def generateKeys():
+    (publicKey, privateKey) = rsa.newkeys(1024)
+    with open('keys/publcKey.pem', 'wb') as p:
+        p.write(publicKey.save_pkcs1('PEM'))
+     with open('keys/privateKey.pem', 'wb') as p:
+        p.write(privateKey.save_pkcs1('PEM'))
+
+def loadKeys():
+    with open('keys/publicKey.pem', 'rb') as p:
+        publicKey = rsa.PublicKey.load_pkcs1(p.read())
+    with open('keys/privateKey.pem', 'rb') as p:
+        privateKey = rsa.PrivateKey.load_pkcs1(p.read())
+    return privateKey, publicKey
+
+def encrypt(message, key):
+    return rsa.encrypt(message.encode('ascii'), key)
+
+def decrypt(ciphertext, key):
+    try:
+        return rsa.decrypt(ciphertext, key).decode('ascii')
+    except:
+        return False
+
+generateKeys()
+publicKey, privateKey =load_keys()
+
+
+key_encypted = encrypt(key, publicKey)
+
+#store the key_encrypted into your database
 
 # value of key is assigned to a variable
 f = Fernet(key)
@@ -69,5 +100,6 @@ if auth_date[3]<timing[3]+1:
     print("valid")
 else:
     print("error")
+
 
 
